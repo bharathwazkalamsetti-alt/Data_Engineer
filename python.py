0fb0e07aac91d9e34672d99fb8ce3f0a74759fbd
bharathwazkalamsetti-alt <bharathwazkalamsetti@gmail.com>
2026-03-01 21:24:19 -0600

    Create python.py

---
+++ python.py
@@ -0,0 +1,25 @@
+# a = ['b_h_a_r_a_t h']
+# b = {'waz','h','a','n','a','z'}
+# print(a)
+# b.remove('waz')
+# b.add('1')
+# print(b)
+# print(len(b))
+# # z = a + b
+# for waz in b:
+#     print(waz)
+# print(z)
+# print(f"{z} is printed")
+
+
+# for i in z:
+#     print(i, end='')
+#     print(f"{i} is printed")
+# print(f"{i} is not printed")
+a = {'name': 'bharat', 'age': 20, 'city': 'delhi'}
+print(a)
+print(type(a))
+print(a['name'])
+print(a.get('age'))
+x = a.values()
+print(x)
